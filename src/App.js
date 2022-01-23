46c27110615290f633fd73a93ad1144aab3fbb1a
ankitt01 <ankitrawat0102@gmail.com>
2022-01-23 12:47:34 +0530

    Bild the shuffling logic and rendered out the images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useState } from 'react'
 import './App.css'
+import SingleCard from './components/SingleCard';
 
+const cardImages = [
+    {"src" : "/img/helmet-1.png" },
+    {"src" : "/img/potion-1.png" },
+    {"src" : "/img/ring-1.png" },
+    {"src" : "/img/scroll-1.png" },
+    {"src" : "/img/shield-1.png" },
+    {"src" : "/img/sword-1.png" },
+]
 function App() {
-  return (
-    <div className="App">
-      <h1>Magic Match</h1>
-      <button>New Game</button>
-    </div>
-  );
+
+    const [cards,setCards] = useState([])
+    const [turns,setTurns] = useState(0);
+
+    //Shuffle cards
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages,...cardImages] //double the cards
+            .sort(() => Math.random()-0.5)  //shuffling
+            .map((card) => ({...card, id: Math.random()}))    //Giving each of them a random id
+
+            setCards(shuffledCards);
+            setTurns(0);
+    }
+    console.log(cards,turns);
+    return ( 
+        <div className = "App">
+            <h1> Magic Match </h1> 
+            <button onClick={shuffleCards}> New Game </button> 
+
+            <div className="card-grid">
+                {cards.map(card => (
+                    <SingleCard key={card.id} card={card} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
